e742a7945eae588b720ccca659af1c2e2f0652c5
Kei-K23 <keiksl2301@gmail.com>
2023-12-23 20:26:49 +0630

    init commit

---
+++ app/Http/Controllers/CategoryApiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        if (!$request->name) {
+            return response(["error" => "Name is required"], 400);
+        }
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->save();
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        if (!$request->name) {
+            return response(["error" => "Name is required"], 400);
+        }
+
+        $category->name = $request->name;
+        $category->update();
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return $category;
+    }
+}
